2cbe0aedd50558ad987508acfa66a259c148af34
DimpalPatil16 <patildimpal727@gmail.com>
2026-09-20 18:06:05 +0530

    C programming

---
+++ dap11.c
@@ -0,0 +1,26 @@
+/*
+Program(11)-> Write a program to check whether a given numbers is even or odd.
+*/
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    int num, i = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("\nMultiplication Table of %d\n", num);
+
+    do
+    {
+        printf("%d x %d = %d\n", num, i, num * i);
+        i++;
+    }
+    while (i <= 10);
+
+    return 0;
+}
